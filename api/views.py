382c83cfedb8ba5e2efab2cd89e4d9a803b8fc38
Ankita Mishra <ankitamishra2617@gmail.com>
2026-02-09 14:32:47 +0530

    Remove unnecessary comments and redundant code

--- api/views.py
+++ api/views.py
@@ -10,16 +10,3 @@ def facts_api(request):
         {"id": 3, "fact": "Earth has one moon."}
     ]
     return Response(facts)
-
-# Create your views here.
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-@api_view(['GET'])
-def facts_api(request):
-    facts = [
-        {"id": 1, "fact": "The sun is a star."},
-        {"id": 2, "fact": "Water boils at 100°C."},
-        {"id": 3, "fact": "Earth has one moon."}
-    ]
-    return Response(facts)
